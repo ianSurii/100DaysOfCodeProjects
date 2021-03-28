d7c6fb6842f089ee6135030348ba868f52877864
IanSurii <mrkeys254@gmail.com>
2021-03-28 22:41:46 +0300

    flutter login

--- FlutterLogin/lib/Screens/login/login.dart
+++ FlutterLogin/lib/Screens/login/login.dart
@@ -1,5 +1,8 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+
  class Login extends StatelessWidget{
 
    Widget build(BuildContext context){
@@ -7,20 +10,73 @@ import 'package:flutter/painting.dart';
          backgroundColor: Colors.white,
           appBar: AppBar(
             title:Text('Login',
-            style: TextStyle( color: Colors.blue,
+            style: TextStyle( color: Colors.white,
                 fontWeight: FontWeight.w500,
                 fontSize: 30),
             ),
-          leading: Text("=="),
+          leading: Text("==",
+          style:TextStyle(color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: 30)),
+
             actions: <Widget>[
-              
+
             ],
 
           ),
+
+     body: Body(),
      );
+
    }
 
 
  }
+class Body extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    Size size = MediaQuery.of(context).size;
 
+    return Container(
+      width: double.infinity,
+      height: size.height,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: double.infinity*0.5,
+            child: Column(
+              children: <Widget>[
+        TextField(
+
+        cursorColor: Colors.green,
+        decoration: InputDecoration(
+
+        hintText:"Hint",
+        border: InputBorder.none,
+    ),
+          ),
+          Container(
+              child: Column(
+                children: <Widget>[
+                  TextField(
+
+                  )
+                ],
+              )
+          ),
+
+
+        ],
+      )
+      ),
+      ],
+    ),
+
+    );
+  }
+
+}
 
